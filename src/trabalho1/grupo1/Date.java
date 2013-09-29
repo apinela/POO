a03eb176307e0e98cdd45c40f931d938722939e5
sheffzor <sheffzor@gmail.com>
2013-09-30 00:09:51 +0100

    Date class implementation

--- src/trabalho1/grupo1/Date.java
+++ src/trabalho1/grupo1/Date.java
@@ -10,10 +10,232 @@
  ******************************************************************************/
 package trabalho1.grupo1;
 
+import java.util.Calendar;
+
 public class Date {
+
+	private static final int[] MONTH_DAYS_COMMON_YEAR = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MONTH_DAYS_LEAP_YEAR = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	private int year;
+	private int month;
+	private int day;
+	private int[] monthDays;
+
+	public Date(int year, int month, int day) {
+
+		this.year = year;
+		this.month = month;
+		this.day = day;
+
+		try {
+			initializeClass();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Date(String str) {
+
+		try {
+
+			/*
+			 * str syntax: DD-MM-YYYY 
+			 * 			   ^^ ^^ ^^^^
+			 *  split map:  0  1    2
+			 */
+
+			String[] tmpDate = str.split("-");
+
+			this.year = Integer.parseInt(tmpDate[2]);
+			this.month = Integer.parseInt(tmpDate[1]);
+			this.day = Integer.parseInt(tmpDate[0]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			initializeClass();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	public Date() {
 		
+		Calendar currDateTime = Calendar.getInstance();
+
+		this.year = currDateTime.get(Calendar.YEAR);
+		this.month = currDateTime.get(Calendar.MONTH);
+		this.day = currDateTime.get(Calendar.DAY_OF_MONTH);
+		
+		try {
+			initializeClass();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
+	public static boolean isLeapYear(int year) {
+
+		if (year % 400 == 0)
+			return true;
+		if (year % 4 == 0 && year % 100 != 0)
+			return true;
+		return false;
+
+	}
+	
+	private void initializeClass() throws Exception {
+		
+		/* Validate Month */
+		if (this.month > 12 && this.month < 1)
+			throw new Exception("Wrong month.");
+		
+		/* Validate Day */
+		
+		if(isLeapYear(this.year)) {
+			this.monthDays = MONTH_DAYS_LEAP_YEAR;
+		} else {
+			this.monthDays = MONTH_DAYS_COMMON_YEAR;
+		}
+		
+		if (this.day > this.monthDays[this.month-1] && this.month < 1)
+			throw new Exception("Wrong day for month and/or year context.");
+		
+	}
+	
+	private static int getLastDayOfMonthByYear(int year, int month) {
+		if(isLeapYear(year)) {
+			return MONTH_DAYS_LEAP_YEAR[month-1];
+		} else {
+			return MONTH_DAYS_COMMON_YEAR[month-1];
+		}
+	}
+	
+	public String toString() {
+		
+		String tmpDay = (String.valueOf(this.day).length() == 1) ? "0" + this.day : Integer.toString(this.day);
+		String tmpMonth = (String.valueOf(this.month).length() == 1) ? "0" + this.month : Integer.toString(this.month);
+		String tmpYear = Integer.toString(this.year);
+
+		return tmpDay+"-"+tmpMonth+"-"+tmpYear;
+	}
+	
+	public boolean equals(Date d) {
+		
+		return this.toString().equals(d.toString());
+		
+	}
+	
+	public int compareTo(Date d) {
+		
+		String[] tmpNewDate = d.toString().split("-");
+		
+		/* Parse date values to integer */
+		
+		int newDay = Integer.parseInt(tmpNewDate[0]);
+		int newMonth = Integer.parseInt(tmpNewDate[1]);
+		int newYear = Integer.parseInt(tmpNewDate[2]);
+		
+		/* Count total number of days for this class */
+		
+		int diffDays = 0;
+		if (this.year > newYear) {
+			for (int yr=newYear; yr<=this.year; yr++) {
+				if (yr==newYear) {
+					for (int mn=newMonth; mn<=12; mn++) {
+						if (mn==newMonth) {
+							for (int dy=newDay; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays--;
+							}
+						} else {
+							for (int dy=1; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays--;
+							}
+						}
+					}
+				} else if (yr==this.year) {
+					for (int mn=1; mn<=this.month; mn++) {
+						if (mn==this.month) {
+							for (int dy=this.day; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays--;
+							}
+						} else {
+							for (int dy=1; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays--;
+							}
+						}
+					}
+				} else {
+					for (int mn=1; mn<=12; mn++) {
+						for (int dy=1; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+							diffDays--;
+						}
+					}
+				}
+			}
+		}
+		if (this.year < newYear) {
+			for (int yr=this.year; yr<=newYear; yr++) {
+				if (yr==this.year) {
+					for (int mn=this.month; mn<=12; mn++) {
+						if (mn==this.month) {
+							for (int dy=this.day; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays++;
+							}
+						} else {
+							for (int dy=1; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays++;
+							}
+						}
+					}
+				} else if (yr==newYear) {
+					for (int mn=1; mn<=newMonth; mn++) {
+						if (mn==newMonth) {
+							for (int dy=newDay; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays++;
+							}
+						} else {
+							for (int dy=1; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+								diffDays++;
+							}
+						}
+					}
+				} else {
+					for (int mn=1; mn<=12; mn++) {
+						for (int dy=1; dy <= getLastDayOfMonthByYear(yr,mn); dy++) {
+							diffDays++;
+						}
+					}
+				}
+			}
+		}
+		if (this.equals(d))
+			return 0;
+		
+		return diffDays;
+	}
+	
+	public Date nextDate() {
+		int tmpDay = this.day;
+		int tmpMonth = this.month;
+		int tmpYear = this.year;
+		
+		if (this.day + 1 > getLastDayOfMonthByYear(this.year,this.month)) {
+			tmpDay=1;
+			if (this.day + 1 > 12) {
+				tmpMonth=1;
+				tmpYear=this.year+1;
+			} else
+				tmpMonth=this.month+1;
+		} else
+			tmpDay=this.day+1;
+		
+		return new Date(tmpDay,tmpMonth,tmpYear);
+	}
+	
 }
---
+++ src/trabalho1/grupo1/DateTest.java
@@ -0,0 +1,91 @@
+package trabalho1.grupo1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DateTest {
+	
+	private static final int[] MONTH_DAYS_COMMON_YEAR = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MONTH_DAYS_LEAP_YEAR = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	@Test
+	public final void testDateIntIntInt() {
+		try {
+			for (int year = 2008; year < 2010; year++) {
+				for (int month = 1; month < 13; month++) {
+					for (int day = 1; day < ((Date.isLeapYear(year)) ? MONTH_DAYS_LEAP_YEAR[month-1] : MONTH_DAYS_COMMON_YEAR[month-1]) + 1; day++ ) {
+						new Date(year,month,day);
+					}
+				}
+			}
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public final void testDateString() {
+		try {
+			for (int year = 2008; year < 2010; year++) {
+				for (int month = 1; month < 13; month++) {
+					for (int day = 1; day < ((Date.isLeapYear(year)) ? MONTH_DAYS_LEAP_YEAR[month-1] : MONTH_DAYS_COMMON_YEAR[month-1]) + 1; day++ ) {
+						new Date(day+"-"+month+"-"+year);
+					}
+				}
+			}
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public final void testDate() {
+		try {
+			new Date();
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public final void testToString() {
+		try {
+			System.out.println("Date.toString: " + new Date().toString());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public final void testEqualsDate() {
+		try {
+		System.out.println("Date.equals: "+new Date().equals(new Date("01-01-2000")));
+		System.out.println("Date.equals: "+new Date().equals(new Date()));
+		System.out.println("Date.equals: "+new Date().equals(new Date("01-01-2020")));
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public final void testCompareTo() {
+		try {
+			System.out.println("Date.compareTo: "+new Date("01-01-2010").compareTo(new Date("01-01-2000")));
+			System.out.println("Date.compareTo: "+new Date().compareTo(new Date()));
+			System.out.println("Date.compareTo: "+new Date("01-01-2010").compareTo(new Date("01-01-2020")));
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public final void testNextDate() {
+		try {
+			System.out.println("Date.nextDate: "+new Date().nextDate());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
